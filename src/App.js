939acd63546fdb02186d97e3953649e28235b0ad
Ankit <ankit628792@gmail.com>
2021-08-22 14:46:52 +0530

    rm unused code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Sidebar from './Components/Sidebar'
 import Service from './Components/Service';
 import Header from './Components/Header';
 import { Switch, Route, useLocation } from 'react-router-dom'
 import About from './Components/About';
+import data from './data'
 
 import AllProjects from './Components/AllProjects';
 import Contact from './Components/Contact';
@@ -15,23 +16,18 @@ import { initialState, reducer } from './reducer';
 export const ThemeContext = createContext();
 
 function App() {
-  const [data, setData] = useState();
   const location = useLocation();
   const [state, dispatch] = useReducer(reducer, initialState)
   const head = document.getElementById('root');
   const imgList = document.getElementsByTagName('img');
-  
+
   if(state){
     head.style.filter = 'invert(1) hue-rotate(100deg) saturate(1.2)';
   }
   else{
     head.style.filter = 'none'
   }
-  
-  useEffect(() => {
-    fetch(`process.env.DataURI`).then(res => res.json()).then(response => setData(response));
-  }, []);
-  console.log('data', data);
+
   return (
     <>
     <ThemeContext.Provider value={{state, dispatch}}>
@@ -40,19 +36,19 @@ function App() {
         <div className="main__inner">
           <Switch location={location} key={location.pathname}>
             <Route exact path="/">
-              <Header data={data[0].personal} />
+              <Header data={data.personal} />
             </Route>
             <Route exact path="/about">
-              <About about={data[0].personal} />
+              <About about={data.personal} />
             </Route>
             <Route exact path="/skills">
-              <Skills skills={data[0].skills} />
+              <Skills skills={data.skills} />
             </Route>
             <Route exact path="/service">
-              <Service services={data[0].service} />
+              <Service services={data.service} />
             </Route>
             <Route exact path="/projects">
-              <AllProjects projects={data[0].projects} />
+              <AllProjects projects={data.projects} />
             </Route>
             <Route exact path="/contact">
               <Contact />
